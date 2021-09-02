f180ec182263131ae19fab5db279f3c7570c21b9
Andrey Codez <andreycodez@bixtrix.com>
2021-09-03 02:32:38 +1000

    Error elems updated

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// For slider color changing in future
+//
+// const rangers = document.querySelectorAll("[type='range']");
+// rangers.forEach( item =>  {
+//     item.addEventListener('input', (e) => {
+//         const value = (this.value-this.min)/(this.max-this.min)*100
+//         e.target.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
+//     }
+// )})
